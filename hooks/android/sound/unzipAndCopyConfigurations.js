fc2265a197d3ed338957d5926318f0e844405189
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2023-04-04 18:20:50 +0100

    refactor: replace var with let

--- hooks/android/sound/unzipAndCopyConfigurations.js
+++ hooks/android/sound/unzipAndCopyConfigurations.js
@@ -4,39 +4,39 @@ var path = require("path");
 var utils = require("./utilities");
 
 module.exports = function(context) {
-  var cordovaAbove8 = utils.isCordovaAbove(context, 8);
-  var defer;
+  let cordovaAbove8 = utils.isCordovaAbove(context, 8);
+  let defer;
   if (cordovaAbove8) {
     defer = require("q").defer();
   } else {
     defer = context.requireCordovaModule("q").defer();
   }
   
-  var platform = context.opts.plugin.platform;
-  var platformConfig = utils.getPlatformConfigs(platform);
+  let platform = context.opts.plugin.platform;
+  let platformConfig = utils.getPlatformConfigs(platform);
   if (!platformConfig) {
     utils.handleError("Invalid platform", defer);
   }
 
-  var sourceFolderPath = platformConfig.getSoundSourceFolder()
-  var destFolderPath = platformConfig.getSoundDestinationFolder()
+  let sourceFolderPath = platformConfig.getSoundSourceFolder()
+  let destFolderPath = platformConfig.getSoundDestinationFolder()
 
   if(!utils.checkIfFolderExists(destFolderPath)) {
     utils.createOrCheckIfFolderExists(destFolderPath)
   }
 
-  var files = utils.getFilesFromPath(sourceFolderPath);
+  let files = utils.getFilesFromPath(sourceFolderPath);
   if (!files) {
     utils.handleError("No directory found", defer);
   }
   else {
-    var filteredFiles = files.filter(function(file){
+    let filteredFiles = files.filter(function(file){
       return file.endsWith(platformConfig.soundFileExtension) == true;
     });
   
     filteredFiles.forEach(function (f) {
-      var filePath = path.join(sourceFolderPath, f);
-      var destFilePath = path.join(destFolderPath, f);
+      let filePath = path.join(sourceFolderPath, f);
+      let destFilePath = path.join(destFolderPath, f);
       utils.copyFromSourceToDestPath(defer, filePath, destFilePath);
     });
   }
