8452be2f0da598a257d131a064fd52a4dbd62586
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2023-04-04 13:49:25 +0100

    refactor: remove unnecessary code from hook
    References: https://outsystemsrd.atlassian.net/browse/RMET-2378

--- hooks/android/sound/unzipAndCopyConfigurations.js
+++ hooks/android/sound/unzipAndCopyConfigurations.js
@@ -5,7 +5,6 @@ var utils = require("./utilities");
 
 module.exports = function(context) {
   var cordovaAbove8 = utils.isCordovaAbove(context, 8);
-  var cordovaAbove7 = utils.isCordovaAbove(context, 7);
   var defer;
   if (cordovaAbove8) {
     defer = require("q").defer();
@@ -22,11 +21,7 @@ module.exports = function(context) {
   var sourceFolderPath = platformConfig.getSoundSourceFolder()
   var destFolderPath = platformConfig.getSoundDestinationFolder()
 
-  console.log("sourceFolderPath: " + sourceFolderPath);
-  console.log("destFolderPath: " + destFolderPath);
-
   if(!utils.checkIfFolderExists(destFolderPath)) {
-    console.log("creating folder bc it does not exist yet.")
     utils.createOrCheckIfFolderExists(destFolderPath)
   }
 
@@ -34,36 +29,17 @@ module.exports = function(context) {
   if (!files) {
     utils.handleError("No directory found", defer);
   }
-
-  console.log("passou getFilesFromPath()");
-
-  var fileName = files.find(function (name) {
-    console.log("name: " + name);
-    console.log("soundFileExtension: " + platformConfig.soundFileExtension);
-    return name.endsWith(platformConfig.soundFileExtension);
-  });
-  if (!fileName) {
-    utils.handleError("No file found", defer);
+  else {
+    var filteredFiles = files.filter(function(file){
+      return file.endsWith(platformConfig.soundFileExtension) == true;
+    });
+  
+    filteredFiles.forEach(function (f) {
+      var filePath = path.join(sourceFolderPath, f);
+      var destFilePath = path.join(destFolderPath, f);
+      utils.copyFromSourceToDestPath(defer, filePath, destFilePath);
+    });
   }
-
-  console.log("passou files.find()");
-
-  console.log("files: " + files);
-
-  var filteredFiles = files.filter(function(file){
-    return file.endsWith(platformConfig.soundFileExtension) == true;
-  });
-
-  console.log("filteredFiles: " + filteredFiles)
-
-  filteredFiles.forEach(function (f) {
-    console.log("currentFile: " + f);
-    var filePath = path.join(sourceFolderPath, f);
-    console.log("filePath: " + filePath);
-    var destFilePath = path.join(destFolderPath, f);
-    console.log("destFilePath: ", destFilePath)
-    utils.copyFromSourceToDestPath(defer, filePath, destFilePath);
-  });
       
   return defer.promise;
 }
