9627a32f3b63139e1ecef9eecc3b5475b0cf77a7
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2023-04-04 14:01:49 +0100

    refactor: remove unnecessary functions
    References: https://outsystemsrd.atlassian.net/browse/RMET-2378

--- hooks/android/sound/utilities.js
+++ hooks/android/sound/utilities.js
@@ -18,19 +18,7 @@ var constants = {
     getSoundSourceFolder: function() {
       return "platforms/android/app/src/main/assets/www";
     }
-  },
-  ios: {
-    platform: "ios",
-    wwwFolder: "www",
-    firebaseFileExtension: ".plist",
-    soundFileName: "push_sound.caf",
-    getSoundDestinationFolder: function(context) {
-      return "platforms/ios/" + utils.getAppName(context) + "/Resources";
-    }
-  },
-  zipExtension: ".zip",
-  folderNameSuffix: ".firebase",
-  folderNamePrefix: "firebase."
+  }
 };
 
 function handleError(errorMessage, defer) {
@@ -52,74 +40,12 @@ function createOrCheckIfFolderExists(path) {
   }
 }
 
-function getSourceFolderPath(context, wwwPath) {
-  var sourceFolderPath;
-  var appId = getAppId(context);
-  var cordovaAbove7 = isCordovaAbove(context, 7);
-
-  // New way of looking for the configuration files' folder
-  if (cordovaAbove7) {
-    sourceFolderPath = path.join(context.opts.projectRoot, "www", appId + constants.folderNameSuffix);
-  } else {
-    sourceFolderPath = path.join(wwwPath, appId + constants.folderNameSuffix);
-  }
-
-  // Fallback to deprecated way of looking for the configuration files' folder
-  if(!checkIfFolderExists(sourceFolderPath)) {
-    console.log("Using deprecated way to look for configuration files' folder");
-    if (cordovaAbove7) {
-      sourceFolderPath = path.join(context.opts.projectRoot, "www", constants.folderNamePrefix + appId);
-    } else {
-      sourceFolderPath = path.join(wwwPath, constants.folderNamePrefix + appId);
-    }
-  }
-
-  return sourceFolderPath;
-}
-
-function getResourcesFolderPath(context, platform, platformConfig) {
-  var platformPath = path.join(context.opts.projectRoot, constants.platforms, platform);
-  return path.join(platformPath, platformConfig.wwwFolder);
-}
-
 function getPlatformConfigs(platform) {
   if (platform === constants.android.platform) {
     return constants.android;
   } else if (platform === constants.ios.platform) {
     return constants.ios;
   }
-}
-
-function getZipFile(folder, zipFileName) {
-  try {
-    var files = getFilesFromPath(folder);
-    for (var i = 0; i < files.length; i++) {
-      if (files[i].endsWith(constants.zipExtension)) {
-        var fileName = path.basename(files[i], constants.zipExtension);
-        if (fileName === zipFileName) {
-          return path.join(folder, files[i]);
-        }
-      }
-    }
-  } catch (e) {
-    console.log(e);
-    return;
-  }
-}
-
-function getAppId(context) {
-  var cordovaAbove8 = isCordovaAbove(context, 8);
-  var et;
-  if (cordovaAbove8) {
-    et = require('elementtree');
-  } else {
-    et = context.requireCordovaModule('elementtree');
-  }
-
-  var config_xml = path.join(context.opts.projectRoot, 'config.xml');
-  var data = fs.readFileSync(config_xml).toString();
-  var etree = et.parse(data);
-  return etree.getroot().attrib.id;
 }
 
 function isCordovaAbove(context, version) {
@@ -129,20 +55,6 @@ function isCordovaAbove(context, version) {
   return parseInt(sp[0]) >= version;
 }
 
-function getAndroidTargetSdk() {
-  var projectPropertiesPath = path.join("platforms", "android", "CordovaLib", "project.properties");
-  if (checkIfFolderExists(projectPropertiesPath)) {
-    var projectProperties = fs.readFileSync(projectPropertiesPath).toString();
-    var lookUp = "target=android-";
-    var from = projectProperties.indexOf(lookUp) + lookUp.length;
-    var length = projectProperties.indexOf('\n', from) - from;
-    var sdk = projectProperties.substr(from, length).trim();
-    console.log('getAndroidTargetSdk', sdk);
-    return parseInt(sdk);
-  }
-
-  throw new Error('Could not find android target in ' + projectPropertiesPath);
-}
 
 function copyFromSourceToDestPath(defer, sourcePath, destPath) {
   fs.createReadStream(sourcePath).pipe(fs.createWriteStream(destPath))
@@ -158,14 +70,9 @@ function copyFromSourceToDestPath(defer, sourcePath, destPath) {
 module.exports = {
   isCordovaAbove,
   handleError,
-  getZipFile,
-  getResourcesFolderPath,
   getPlatformConfigs,
-  getAppId,
   copyFromSourceToDestPath,
   getFilesFromPath,
   createOrCheckIfFolderExists,
-  checkIfFolderExists,
-  getAndroidTargetSdk,
-  getSourceFolderPath
+  checkIfFolderExists
 };
