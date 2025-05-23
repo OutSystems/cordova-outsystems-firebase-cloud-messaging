9f66c6abeca2fd4edd684d5f99ede3af92243816
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2025-05-23 12:21:41 +0100

    fix: Do not use plugin's AppDelegate for Capacitor builds.
    Those builds will need to use the AppDelegate from the actual Capacitor app, as explained in the added comment

--- src/ios/AppDelegate+OSFirebaseCloudMessaging.m
+++ src/ios/AppDelegate+OSFirebaseCloudMessaging.m
@@ -5,6 +5,12 @@
 @implementation AppDelegate (OSFirebaseCloudMessaging)
 
 + (void)load {
+    Class capacitorClass = NSClassFromString(@"Capacitor.CAPBridge");
+    if (capacitorClass) {
+        // Capacitor App - This plugin will be included as a Pod, and so the below app delegate methods won't be called
+        //  Instead, the calls below are injected into the application's AppDelegate for Capacitor iOS apps
+        return;
+    }
     Method original = class_getInstanceMethod(self, @selector(application:didFinishLaunchingWithOptions:));
     Method swizzled = class_getInstanceMethod(self, @selector(application:firebaseCloudMessagingPluginDidFinishLaunchingWithOptions:));
     method_exchangeImplementations(original, swizzled);
