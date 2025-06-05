f378a53c73da0e2812e50cb150c74c5735fc8655
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2025-06-05 12:14:56 -0300

    refactor: remove commented code
    References: https://outsystemsrd.atlassian.net/browse/RMET-4025

--- hooks/android/androidCopyChannelInfo.js
+++ hooks/android/androidCopyChannelInfo.js
@@ -1,50 +1,30 @@
 const path = require('path');
 const fs = require('fs');
-//const et = require('elementtree');
 const { DOMParser, XMLSerializer } = require('xmldom');
 const { ConfigParser } = require('cordova-common');
 
 module.exports = function (context) {
+    // get notification channel name and description from config.xml file
     var projectRoot = context.opts.cordova.project ? context.opts.cordova.project.root : context.opts.projectRoot;
     var configXML = path.join(projectRoot, 'config.xml');
     var configParser = new ConfigParser(configXML);
     var channelName = configParser.getPlatformPreference("NotificationChannelDefaultName", "android");
     var channelDescription = configParser.getPlatformPreference("NotificationChannelDefaultDescription", "android");
 
+    // load strings.xml using DOMParser
     var stringsXmlPath = path.join(projectRoot, 'platforms/android/app/src/main/res/values/strings.xml');
     var stringsXmlContents = fs.readFileSync(stringsXmlPath).toString();
-    
-    //var etreeStrings = et.parse(stringsXmlContents);
-
-    /*
-    var dataTags = etreeStrings.findall('./string[@name="notification_channel_name"]');
-    for (var i = 0; i < dataTags.length; i++) {
-        var data = dataTags[i];
-        data.text = channelName;
-    }
-
-    var dataTagsSecond = etreeStrings.findall('./string[@name="notification_channel_description"]');
-    for (var i = 0; i < dataTagsSecond.length; i++) {
-        var data = dataTagsSecond[i];
-        data.text = channelDescription;
-    }
-
-    var resultXmlStrings = etreeStrings.write();
-    fs.writeFileSync(stringsXmlPath, resultXmlStrings);
-
-    */
-
-    // Parse the XML
     const parser = new DOMParser();
     const xmlDoc = parser.parseFromString(stringsXmlContents, "application/xml");
 
-    // Define target strings and their new values
+    // set strings to replace (channelName and channelDesciption from config.xml)
     const replacements = {
         notification_channel_name: channelName,
         notification_channel_description: channelDescription,
     };
 
-    // Remove duplicates for each key
+    // remove duplicates for each key
+    // this is necessary for builds where the hook runs more than once, to avoid duplicate <string> entries
     const allStrings = Array.from(xmlDoc.getElementsByTagName('string'));
     for (const name of Object.keys(replacements)) {
         allStrings
@@ -52,7 +32,7 @@ module.exports = function (context) {
             .forEach(el => el.parentNode.removeChild(el));
     }
 
-    // Add new <string> elements
+    // add new <string> elements for channelName and channelDescription, using replacements object
     for (const [name, value] of Object.entries(replacements)) {
         const newString = xmlDoc.createElement('string');
         newString.setAttribute('name', name);
@@ -60,10 +40,8 @@ module.exports = function (context) {
         xmlDoc.documentElement.appendChild(newString);
     }
 
-    // Serialize back to string
+    // write back to strings.xml file
     const serializer = new XMLSerializer();
     const updatedXmlString = serializer.serializeToString(xmlDoc);
-    
-    // Write it back to the file
     fs.writeFileSync(stringsXmlPath, updatedXmlString);
 };
